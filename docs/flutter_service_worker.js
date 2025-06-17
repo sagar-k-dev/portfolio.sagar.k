77e153c8c8b31e24fca8a0bd0acfb2b1a6a1de38
sagar_k <108513778+sagarappscrip@users.noreply.github.com>
2025-06-17 17:25:28 +0530

    Fixed web loading issues

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "fedc94d4f74fa17a1b6d6c2293e0b3fa",
+const RESOURCES = {"flutter_bootstrap.js": "63328fa5b28c8602eae1da7c1189c6f0",
 "version.json": "009c9e65172e010890f7f65fde438006",
-"index.html": "46a7a86334a4100e396297d9d7e6f63d",
-"/": "46a7a86334a4100e396297d9d7e6f63d",
+"index.html": "53edda0c3a83c16a32116c732287e455",
+"/": "53edda0c3a83c16a32116c732287e455",
 "main.dart.js": "9e9145e50ade2df17b91496359a7a70c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "aa72129187844fa0db0b12f6440cae2a",
